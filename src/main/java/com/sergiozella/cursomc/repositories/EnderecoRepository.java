80885385f3757ae35a7ff9a1c6950858d9a5f36c
Sergio Zella <sergioluiz.zella@gmail.com>
2020-07-22 19:58:27 -0300

    Cliente, TipoCliente, Telefones e Enderecos

---
+++ src/main/java/com/sergiozella/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.sergiozella.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sergiozella.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
