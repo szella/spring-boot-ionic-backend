0969e7cd90fc148ed847a67dfdb7b3995bda5537
Sergio Zella <sergioluiz.zella@gmail.com>
2020-07-26 15:35:48 -0300

    Endpoint /pedido/{id} disponibilizado

--- src/main/java/com/sergiozella/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/sergiozella/cursomc/domain/PagamentoComBoleto.java
@@ -5,13 +5,17 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sergiozella.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataVencimanto;
+
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataPagamento;
 
 	public PagamentoComBoleto() {
