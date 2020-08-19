86ee20e21d06436966a9ed0707fdb61bf0ab8c36
Zella <sergioluiz.zella@gmail.com>
2020-08-18 21:51:26 -0300

    Informando versao do java para o heroku

--- src/main/java/com/sergiozella/cursomc/config/DevConfig.java
+++ src/main/java/com/sergiozella/cursomc/config/DevConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.sergiozella.cursomc.services.DBService;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class DevConfig {
 
 	@Autowired
